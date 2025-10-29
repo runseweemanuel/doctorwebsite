939ac02847deaddc3aaa175ed411685d8c0f129d
runseweemanuel <runseweemanuel@gmail.com>
2025-10-29 10:58:08 +0100

    push

---
+++ index.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('.menu-holder')
+let drop = document.querySelector('.drop')
+let clog = document.querySelector('.clog')
+
+
+
+menu.addEventListener("click", ()=> {
+    drop.classList.toggle("clog");
+} );
